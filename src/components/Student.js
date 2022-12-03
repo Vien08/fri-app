3c0cca3c4486d6570f96554007cd77fdb36098b1
Vien08 <113112152+Vien08@users.noreply.github.com>
2022-12-03 12:11:39 +0800

    14th commit

--- src/components/Student.js
+++ src/components/Student.js
@@ -23,8 +23,6 @@ function Student(){
       })
     },[])
 
-
-
     const submitBtn = function(e){
       e.preventDefault();
       let getData = new FormData ();
@@ -86,7 +84,7 @@ function Student(){
     return(
       <>
         <h1>Student's List</h1>
-        <form action="">
+        <form>
           <input type="text" name="fname" value= {fname} onChange = {(e) => setFname(e.target.value)}/>
           <input type="text" name="lname" value= {lname} onChange = {(e) => setLname(e.target.value)}/>
           <input type="number" name="age" value= {age} onChange = {(e) => setAge(e.target.value)}/>
