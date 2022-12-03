7c1864b83bca088da6868779db31565a180fdcae
Vien08 <113112152+Vien08@users.noreply.github.com>
2022-12-03 11:16:46 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar/>
         <div>
           <Routes>
-            <Route path="/react-app" element={<Home/>} />
+            <Route path="/home" element={<Home/>} />
             <Route path="/features" element={<Features/>}/>
             <Route path="/pricing" element={<Pricing/>}/>
           </Routes>
